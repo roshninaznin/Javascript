021a2d113bf879686db86c08bb56747d2599ab8a
Naznin Akter Roshmny <147531288+roshninaznin@users.noreply.github.com>
2025-11-30 20:59:06 +0600

    Add files via upload

---
+++ Data type.js
@@ -0,0 +1,32 @@
+// data types
+
+// primitive and non-primitive
+
+// primirtive [number. string, boolean, undefined, null, bigInt, symbol]
+
+//1. number
+let age = 23;
+let price = 100.5;
+
+//2.string
+
+fullName= "naznin Roshni"
+
+//3.boolean
+isFollow = true;
+
+//4.undefined  // no value
+
+
+let x;
+
+
+//5.null
+
+
+// 6.bigInt
+let a=BigInt("123");
+
+// 7.symbol
+
+let y=Symbol("Hello");
