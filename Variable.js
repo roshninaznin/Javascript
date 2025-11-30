057e38cea78b963623bd71fc8e5f666f7880fff5
Naznin Akter Roshmny <147531288+roshninaznin@users.noreply.github.com>
2025-11-30 18:34:21 +0600

    Add files via upload

---
+++ Variable.js
@@ -0,0 +1,23 @@
+
+// variable
+fullName="Naznin Roshni";
+console.log(fullName);
+
+age=24;
+console.log(age);
+
+price=99.99;
+radius=14;
+a=null;
+b=undefined;
+console.log(b);
+ isFollow=true; //boolean; true/false
+console.log(isFollow);
+
+// let/const/var [variable defined time used]
+
+let num = 45;
+console.log(num);
+
+const id=22;
+console.log(num);
