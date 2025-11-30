aad0e42539e30304ed3ff4b4eadbbb79c4dba8e7
Naznin Akter Roshmny <147531288+roshninaznin@users.noreply.github.com>
2025-12-01 03:33:49 +0600

    Add files via upload

---
+++ Non primitive Data type.js
@@ -0,0 +1,27 @@
+// non primitive data type
+//we can say them objects
+// two types are there: array, functions
+
+
+// object create:    object={key:value, key:value}
+
+const student = {
+    fullname: "Naznin",
+    Age: 23,
+    cgpa: 3.80,
+    isPassed: true,
+}
+
+// two ways of access value for print
+console.log(student.Age);
+console.log(student["fullname"]);
+
+console.log(typeof student);
+console.log(typeof student["fullname"]);
+console.log(typeof student["age"]);
+console.log(typeof student["cgpa"]);
+console.log(typeof student["isPassed"]);
+
+
+
+  
