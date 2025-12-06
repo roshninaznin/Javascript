8b8e53fe2d2ff1a18a78e2f462133379f535a65d
Naznin Akter Roshmny <147531288+roshninaznin@users.noreply.github.com>
2025-12-06 08:52:54 +0600

    Update unary_operator.js

--- unary_operator.js
+++ unary_operator.js
@@ -10,3 +10,20 @@ b--;
 b--;
 console.log(b);
 
+//pre and post
+
+let x = 5;
+let y = 4;
+//pre
+console.log("++x =", ++x);
+console.log("++y =", ++y);
+
+let m = 5;
+let n = 4;
+//post
+console.log("m++ =", m++);
+console.log("m =", m);
+
+console.log("n++ =", n++);
+console.log("n =", n);
+
