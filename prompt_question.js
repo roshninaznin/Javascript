4db9be6dcf0ddb539dea62c4013fb6b8df4badd6
Naznin Akter Roshmny <147531288+roshninaznin@users.noreply.github.com>
2025-12-06 18:24:16 +0600

    Update prompt_question.js

--- prompt_question.js
+++ prompt_question.js
@@ -3,7 +3,7 @@
 //get user input a number using prompt('Enter a number:). Check if the number is multiple of 5 or not.
 let num = prompt("Enter a number:");
 
-if(num%5 ==0){
+if(num%5 ===0){
     console.log(num,"is multiple of 5.");
 }
 else{
