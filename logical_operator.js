4c6cb92f04633846b8c6bbf9d99d7bc902c906ca
Naznin Akter Roshmny <147531288+roshninaznin@users.noreply.github.com>
2025-12-06 18:07:15 +0600

    Create logical_operator.js

---
+++ logical_operator.js
@@ -0,0 +1,21 @@
+// bitwise operator
+
+let a= 5;
+let b= 10;
+
+//&&
+let cond1 = b>a;
+let cond2 = a===5;
+
+console.log("cond1 && cond2", cond1 && cond2);
+
+//||
+
+let x=5;
+let y=2;
+
+//&&
+let condt1 = x==y;
+let condt2 = x===5;
+
+console.log("condt1 && condt2", condt1 && condt2);
