8e6b674ce9bca952954d6bb90a7f080bb05fa419
Naznin Akter Roshmny <147531288+roshninaznin@users.noreply.github.com>
2025-12-06 17:50:24 +0600

    Create assignment_operator.js

---
+++ assignment_operator.js
@@ -0,0 +1,23 @@
+//assign operator
+let a=5;
+let b=10;
+
+// value check
+console.log("a==b", a==b); //false
+console.log("a!=b", a!=b); //true
+
+//data type check
+let x="5";//string
+let y="5";//int
+// data type check
+console.log("x===y", x===y); //true
+console.log("x!==y", a!==x); //false
+
+let m=6;
+let n=5;
+// >, <, >=, <=
+console.log("6>5:", 6 > 5);
+console.log("5>6:", 5 > 6);
+
+console.log("6>=6:", 6 >= 6);
+console.log("4<=4:", 4 <= 4);
