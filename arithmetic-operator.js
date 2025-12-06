b1e917ee5557d9706d3c1e3ef1e4a278bfa3a355
Naznin Akter Roshmny <147531288+roshninaznin@users.noreply.github.com>
2025-12-06 08:41:16 +0600

    Create arithmetic-operator.js

---
+++ arithmetic-operator.js
@@ -0,0 +1,9 @@
+//    arithmetic operator
+let a= 10;
+let b = 5;
+console.log("a=", a, "and b=",b);
+console.log("a +b = ",a+b);
+console.log("a-b=", a-b);
+console.log("a-b=", a*b);
+console.log("a-b=", a/b);
+console.log("a-b=", a/b);
